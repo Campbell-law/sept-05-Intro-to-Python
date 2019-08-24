85190301a857957d39bc665ae3f1308dbd892f98
tmb <tom.brooke@gmail.com>
2019-08-24 12:44:16 -0400

    Finished exercises

--- sept-05.py
+++ sept-05.py
@@ -1,7 +1,10 @@
+
 # Intro to Python Chapter #1
 
+# Lines that begin with # are comments -- they explain code but are not executed
+
 # Basic Math at the repl 
-# You cans start repl on command line or in Vs-Code - shift -comand on mac then (Python_start repl)
+# You cans start repl on command line or in VS-Code - shift -comand on mac then (Python_start repl)
 # 1. what is the result of: 2 + 3 * 5
 
 # play around with different math functions at repl
@@ -20,7 +23,7 @@ n = 2 + 3 * 5
 
 print(n)
 
-# 2. assign 2,3 and 5 each to different variables then add/multiply them together in th print() function
+# 2. assign 2,3 and 5 each to different variables then add/multiply them together in the print() function
 
 # 3. same as 2 but assign the answer to a variable and print that
 
@@ -33,11 +36,11 @@ print(n)
 
 type(2)
 
-# 7. find type for 2.5
+# 7. Find type for 2.5
 
 type(2.5)
 
-# 8. Type for 2 + 2.5
+# 8. Find type for 2 + 2.5
 
 2 + 2.5
 type(2+2.5)
@@ -54,15 +57,44 @@ type(2+2.5)
 
 l = "law"
 
+
 # 9. print() Campbell Law School
+#  a) All three words as a single string
+#  b) use concatenated strings in a print statement print("Campbell" + ...)
+#  c) assign to a variable and print the variable
+
 
 # 10. What happens when you print() "Law" + 2019  (How can you fix it?)
 
 
 
+# 11. What about "Campbell" * 3 ?
+
+
+# Variables:
+#  1) Can only be one word
+#  2) Can't begin with number
+#  3) Hyphens not allowed 
+#  4) Can't be reserved words
+#  5) Can't be special character like $ of '
+
+# 12. which ones of the following are valid:
+
+# school = "campbell Law"
+# 2L = "Bob"
+# law-school = "Campbell"
+# total_$um = 100
+# not = "no"
+
+# 13. Write "Your First Program" page 21 in text (Hint - You can copy it)
 
 
 
+# 14. What happens with print("I am " + 25 + " years old") - Can you fix it
 
 
-
+# 15. Which of the following are True
+   a). 25 == '25'
+   b). 25 == 25.00
+   c). 25 == 0025.00
+   d). 25 == int("25")
